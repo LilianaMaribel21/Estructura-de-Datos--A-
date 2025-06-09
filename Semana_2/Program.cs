c45bbb3bfb0c0e415844150fabfd1ae5fd956e0c
LilianaMaribel21 <lm.bravoc@uea.edu.ec>
2025-06-08 21:31:05 -0500

    .

--- Semana_2/Program.cs
+++ Semana_2/Program.cs
@@ -1,15 +1,15 @@
-﻿﻿// Ejemplo de uso
+﻿// Ejemplo de uso
 class Program
 {
     static void Main(string[] args)
     {
         // Crear un objeto de la clase Circulo
-        Circulo circulo = new Circulo(5);
+        Circulo circulo = new Circulo(9);
         Console.WriteLine($"Área del círculo: {circulo.CalcularArea()}"); // Ejemplo de cálculo del área
         Console.WriteLine($"Perímetro del círculo: {circulo.CalcularPerimetro()}"); // Ejemplo de cálculo del perímetro
 
         // Crear un objeto de la clase Rectangulo
-        Rectangulo rectangulo = new Rectangulo(4, 7);
+        Rectangulo rectangulo = new Rectangulo(6, 14);
         Console.WriteLine($"Área del rectángulo: {rectangulo.CalcularArea()}"); // Ejemplo de cálculo del área
         Console.WriteLine($"Perímetro del rectángulo: {rectangulo.CalcularPerimetro()}"); // Ejemplo de cálculo del perímetro
     }
